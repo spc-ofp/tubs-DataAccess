53e0bce4540d6526ff6d31e8ca1dd8e0a768d803
Oceanic Fisheries Programme <coreyc@spc.int>
2011-10-18 16:07:31 +1100

    Comments fix.

--- src/main/java/org/spc/ofp/tubs/domain/purseseine/FishingSet.java
+++ src/main/java/org/spc/ofp/tubs/domain/purseseine/FishingSet.java
@@ -1,19 +1,20 @@
 /*
  * Copyright (C) 2011 Secretariat of the Pacific Community
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This file is part of TUBS.
  *
- * This program is distributed in the hope that it will be useful,
+ * TUBS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TUBS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with TUBS.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.spc.ofp.tubs.domain.purseseine;
 
